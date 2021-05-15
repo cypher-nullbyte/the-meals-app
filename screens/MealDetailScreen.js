8f5d4cdb22bc99b0fd5a4499e0a7a57d98a03cae
cYpHeR <64411912+cypher-nullbyte@users.noreply.github.com>
2021-05-15 19:41:50 +0530

    Adding Start Button to Header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import {HeaderButtons,Item} from 'react-navigation-header-buttons';
+import CustomHeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen=props=>{
+    const mealId=props.navigation.getParam('mealId');
+    const selectedMeal=MEALS.find(meal=>meal.id===mealId);
     return (
         <View style={styles.screen}>
-            <Text>The MealDetailScreen!</Text>
+            <Text>{selectedMeal.title}</Text>
             <Button title="Go Back to Categories!" onPress={()=>{props.navigation.popToTop();}}/>
         </View>
     )};
-    
+MealDetailScreen.navigationOptions=(naviationData)=>{
+    const mealId=naviationData.navigation.getParam('mealId');
+    const selectedMeal=MEALS.find(meal=>meal.id===mealId);
+    return{
+        headerTitle:selectedMeal.title,
+        headerRight:()=>{
+            return (
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item title="Fav" iconName='ios-star' onPress={()=>{console.log("FAV ADDED")}}/>
+            </HeaderButtons>
+            );
+        }
+      };
+};
     
 const styles = StyleSheet.create({
     screen:{
